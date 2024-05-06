7b4abce9c150461a0f8570ad868226aacbf691d2
saidabdul80 <saidabdulsalam5@gmail.com>
2024-05-06 11:24:10 +0100

    fix: updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts/master')
 @section('content')
 <br><br>
-<div class=" px-4 py-2 mt-5 bg-white rounded-lg d-flex flex-column align-items-center justify-content-center " style="height: 78.1vh;">
+<div class=" px-4 py-2 mt-5 bg-white rounded-lg d-flex flex-column align-items-center justify-content-center " style="height: 75.1vh;">
     <img src="{{asset('images/nicare_logo.jpg')}}" style="width: 135px;" >
     <h1 class="text-secondary fw-bold">NiCare Fund Management System (NiFMS)</h1>
 <div>
-    <span style="font-size: 10px; text-align:center">
+    <span style="font-size: 10px; text-align:center padding:14px;">
         All Rights Reserved, Copyright 202,IHSAN Technologies
     </span>
 @endsection
